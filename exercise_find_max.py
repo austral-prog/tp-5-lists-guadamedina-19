7a8dd0a34f045157c29f7a80c5a34209452458fd
guadamedina-19 <mgmedinarusso@mail.austral.edu.ar>
2026-04-14 12:53:32 -0300

    Update exercise_find_max.py

--- exercise_find_max.py
+++ exercise_find_max.py
@@ -11,7 +11,7 @@ def find_max(lista):
     Returns:
         El valor máximo de la lista o None si está vacía
     """
-    pass  # Reemplazar con tu implementación
+    
     if lista != [ ]:
         return max(lista)
     elif lista == [ ]:
